29e7082498e644fdfe9710d9e6bedc96e0f7af65
Ahmed Hesham <ahmdhesham241@gmail.com>
2022-08-23 21:35:35 +0200

    commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return(
+        <nav>
+        <img src="./src/react-icon-small.png" className="nav-logo-icon" />
+        <h3 className="nav-logo-text">ReactFacts</h3>
+        <h4 className="nav-title">React Course Project -1</h4>
+    </nav>
+    )
+}
+
+export default Navbar
